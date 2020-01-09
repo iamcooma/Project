9afd5057637f9c250bd0d7d30e1bb5c077dac6ee
iamcooma <bus8912345@gmail.com>
2020-01-09 10:54:30 +0700

    edit flie

--- view/download.php
+++ view/download.php
@@ -1,5 +1,5 @@
 <?php 
-    header("Content-disposition: attachment; filename=Krukiddee.docx");
+    header("Content-disposition: attachment; filename=taejai_project_submission.docx");
     header("Content-type: application/vnd.openxmlformats-officedocument.wordprocessingml.document");
-    readfile("Krukiddee.docx");
+    readfile("taejai_project_submission.docx");
 ?>
