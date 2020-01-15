68944fd347a06c0b34f81964df3448ca30d8e611
iamcooma <bus8912345@gmail.com>
2020-01-15 14:26:19 +0700

    news now

--- shared/footer.php
+++ shared/footer.php
@@ -12,8 +12,9 @@
                 </div>
                 <div class="col-lg-4  col-md-6 col-sm-6">
                     <div class="single-footer-widget">
-                        <h6>ความเคลื่อนไหว<h6>
-                        <p>อัพเดทข่าวสาร ทุกวัน.<p>
+                        <h6>สอบถามและติดต่อเพิ่มเติม<h6>
+                        <p>Email :Krukiddee@gamil.com<p>
+                        <p>Phone :xxx-xxx-xxx<p>
                         <div class="" id="mc_embed_signup">
 
                             <!-- <form target="_blank" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="form-inline"> -->
