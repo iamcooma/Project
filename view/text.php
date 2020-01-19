50310d7cc09c4db89fd3282f1338e54e9ff3c1e0
iamcooma <bus8912345@gmail.com>
2020-01-19 22:40:33 +0700

    new file of data

--- view/text.php
+++ view/text.php
@@ -7,8 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <?php include('../shared/style.php'); ?>
 </head>
-
+<!-- card style-->
 <style>
     .card {
         margin: 0 auto;
@@ -19,122 +20,25 @@
         /* Added */
         margin-right: 10px;
     }
-
-    element.style {}
-
-    .fund-raising>.body {
-        padding: 30px 25px 0 25px;
-    }
-
-    * {
-        -webkit-box-sizing: border-box;
-        -moz-box-sizing: border-box;
-        box-sizing: border-box;
-    }
-
-    user agent stylesheet div {
-        display: block;
-    }
-
-    .fund-raising {
-        font-family: "kanitlight", Helvetica Neue, Helvetica, Arial, sans-serif;
-        font-weight: 100;
-        -webkit-border-radius: 6px;
-        -moz-border-radius: 6px;
-        border-radius: 6px;
-        background-clip: padding-box;
-        color: #666;
-        border: 1px solid #f3d9ab;
-        background-color: #fff;
-        z-index: 999;
-    }
-
-    body {
-        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-        font-size: 16px;
-        font-weight: 400;
-        line-height: 1.7em;
-        color: #393939;
-    }
-
-    body {
-        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-        font-size: 14px;
-        line-height: 1.42857143;
-        color: #333;
-        background-color: #fff;
-    }
-
-    html {
-        font-size: 10px;
-        -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-    }
-
-    html {
-        font-family: sans-serif;
-        -webkit-text-size-adjust: 100%;
-        -ms-text-size-adjust: 100%;
-    }
-
-    user agent stylesheet html {
-        color: -internal-root-color;
-    }
-
-    .fund-raising>.body:before,
-    .fund-raising>.body:after {
-        content: " ";
-        display: table;
-    }
-
-    :after,
-    :before {
-        -webkit-box-sizing: border-box;
-        -moz-box-sizing: border-box;
-        box-sizing: border-box;
-    }
-
-    .fund-raising>.body:after {
-        clear: both;
-    }
-
-    .fund-raising>.body:before,
-    .fund-raising>.body:after {
-        content: " ";
-        display: table;
-    }
-
-    :after,
-    :before {
-        -webkit-box-sizing: border-box;
-        -moz-box-sizing: border-box;
-        box-sizing: border-box;
-    }
 </style>
 
 <body>
-    <h1>โครงเรื่อง</h1>
-    <div class="card text-center">
-        <div class="card-header">
-            <ul class="nav nav-tabs card-header-tabs">
-                <li class="nav-item">
-                    <a class="nav-link active" href="#">Active</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
-                </li>
-            </ul>
-        </div>
-        <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>
+    <?php include('../shared/header-user.php'); ?>
+    <ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+  </li>
+</ul>
 
-    <!-- การบริจาคเด้อ -->
     <div class="container ml-auto">
         <div class="col-mr-6">
             <div class="card text-white bg-dark mb-3" style="width: 18rem;">
@@ -143,6 +47,7 @@
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
+
                     <div class="share-group">
                         <h3 class="title">แชร์ให้พื่อน</h3>
                         <ul class="share-list">
@@ -152,11 +57,14 @@
                             <li class="item"><a class="line link js-share-line" href="#"><span class="icon-line"><span class="hide-txt">line</span></span></a></li>
                         </ul>
                     </div>
+
                 </div> <!-- fund-raising -->
             </div>
         </div>
     </div>
-
+    <hr>
+    <?php include('../shared/footer.php'); ?>
+    <?php include('../shared/script.php'); ?>
 </body>
 
 </html>
--- view/user-index.php
+++ view/user-index.php
@@ -15,56 +15,10 @@ include('../database/condb.php');
 
 <body>
     <?php include('../shared/header-user.php'); ?>
-
+    <hr>
     <div class="container-fluid">
-     <?php include('../components/carousel.php'); ?>
+        <?php include('../components/carousel.php'); ?>
     </div>
-    
-    <?php
-    $sql = "SELECT * FROM `project` WHERE user_id IS NOT NULL ORDER BY project_id DESC ";
-    $result = mysqli_query($conn, $sql);
-    $result2 = mysqli_query($conn, $sql);
-
-    ?>
-
-    <header>
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <?php $i = 0;
-                while ($data = mysqli_fetch_array($result)) { ?>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="<?= $i ?>" <?php if ($i == 0) {
-                                                                                                echo 'class="active"';
-                                                                                            } ?>></li>
-                <?php $i++;
-                } ?>
-            </ol>
-            <div class="carousel-inner" role="listbox">
-
-                <?php $i = 0;
-                while ($data = mysqli_fetch_array($result2)) { ?>
-                    <div class="carousel-item <?php if ($i == 0) {
-                                                    echo 'active';
-                                                } ?>" style="background-image: url('../img/<?= $data['project_pic'] ?>')">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h3 class="display-4"><?= $data['project_title'] ?></h3>
-                            <p class="lead"><?= $data['project_detail'] ?></p>
-                            <a href="form.php?p=<?= base64_encode($data['project_id']) ?>" class="btn btn-warning btn-lg">ร่วมบริจาคโครงการ</a>
-                        </div>
-                    </div>
-                <?php $i++;
-                } ?>
-
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
-    </header>
 
     <!-- Page Content -->
     <section class="py-5">
@@ -72,8 +26,28 @@ include('../database/condb.php');
             <h1 class="font-weight-light">ข่าวสารและเรื่องราว</h1>
             <p class="lead">
                 <dd>หากคุณเป็นคนหนึ่งที่เคยผ่านการศึกษาแบบไทยต้องเคยเจอกิจกรรมหน้าเสาธงตอนเช้าก่อนเข้าเรียนอย่างแน่นอน สิ่งนี้เกิดขึ้นยาวนานตั้งแต่ยุคคุณพ่อคุณแม่เราจวบจนปัจจุบัน เด็กหลายๆ คนเลือกที่จะอดทนต่อสิ่งที่ทำต่อกันมาโดยไม่ปริปากแม้บางครั้งไม่เห็นด้วย แต่หากเริ่มส่งผลกระทบต่อชีวิตทางการศึกษาแล้วล่ะก็ คงถึงเวลาปกป้องสิทธิของตัวเอง “หนูมองว่าทำไมหลักสูตรการเรียนการสอนถึงมากขนาดนี้ แล้วยังมีกิจกรรมหน้าเสาธงในตอนเช้าที่กินเวลาเรียนเราเข้ามาเพิ่มอีก ทำให้ต้องมีการเพิ่มคาบเรียน เพราะเรียนไม่ทัน ไม่มีเวลาอ่านหนังสือสอบ ไม่มีความรู้ไปทำข้อสอบ” นี่คือสิ่งที่น้องมิ้นท์และเพื่อนๆ ลุกขึ้นมาเรียกร้องต่อสู้กับกิจกรรมหน้าเสาธงของโรงเรียนผ่านวิชาของ “ครูกั๊ก” (วิชาความคิดรวบยอดทางคณิตศาสตร์) ที่ให้นักเรียนได้ใช้ความรู้ที่มีมาประยุกต์กับสถานการณ์ต่างๆ ตามโจทย์ รวมถึงสอนให้เด็กรู้จักแก้ปัญหาที่เกิดขึ้นในสังคม โดยเริ่มจากสิ่งที่ใกล้ตัวที่สุดอย่าง “โรงเรียน”</dd>
-                <a href="../view/message.php">ข่าวสารเพิ่มเติม</a>!
+                <a href="#">ข่าวสารเพิ่มเติม</a>!
             </p>
+        </div>
+        <div class="owl-carousel owl-theme blog-slider">
+            <?php
+            $sql = "SELECT * FROM `project` ";
+            $result = mysqli_query($conn, $sql);
+            while ($data = mysqli_fetch_array($result)) {
+
+            ?>
+                <div class="card blog__slide text-center">
+                    <div class="blog__slide__img">
+                        <img class="card-img rounded-0" src="/project/img/<?php echo $data['project_pic']; ?>" alt="...">
+                    </div>
+                    <div class="blog__slide__content">
+                        <a class="blog__slide__label" href="">Fashion</a>
+                        <h3><a href="#">New york fashion week's continued the evolution</a></h3>
+                        <p>2 days ago</p>
+                    </div>
+                </div>
+            <?php } ?>
+
         </div>
     </section>
     <?php include('../shared/footer.php'); ?>
